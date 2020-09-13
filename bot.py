83f89c2a13bc3b94da3bc3e836e9e2560ae47898
SirFrankieCrisp <sirfrankiecrisp@pm.me>
2020-09-13 19:47:47 +0200

    Add status to bot

--- bot.py
+++ bot.py
@@ -1,6 +1,9 @@
 import discord
 import random
-from discord.ext import commands
+import os
+from discord.ext import commands, tasks
+from itertools import cycle
+
 
 #prefix to invoke bot is franz
 client = commands.Bot(
@@ -11,7 +14,8 @@ client = commands.Bot(
 #when the bot is ready
 @client.event
 async def on_ready():
-    print('Bot is ready.')
+    await client.change_presence(status=discord.Status.online, activity=discord.Game('Ready to answer your demands'))
+    print('Franz is ready')
 
 #member joins the server
 @client.event
@@ -23,7 +27,24 @@ async def on_member_join(member):
 async def on_member_remove(member):
     print(f'(member) has removed the server.')
 
+###################### Tasks ########################
 
+#@tasks.loop(seconds=20)
+#async def change_status():
+
+###################### Cogs #########################
+#@client.command()
+#async def load(ctx, extension):
+#    client.load_extension(f'cogs.{extension}')
+#
+#@client.command()
+#async def unload(ctx, extension):
+#    client.unload_extension(f'cogs.{extension}')
+#
+#for filename in os.listdir('./cogs'):
+#    if filename.endswith('.py'):
+#        client.load_extension(f'cogs.{filename[:-3]}') #remove last 3 chars from name
+#
 ###################### Commands ######################
 #ping command responds with latency of Bot
 @client.command()
@@ -97,4 +118,4 @@ async def _8ball(ctx, *, question):
 
 
 
-client.run()
+client.run('NzQ5NjcwODIzMDM5NzI5NzU2.X0vXXA.Yqbz2U75rpIVf1w0uJVMLmefVjg')
